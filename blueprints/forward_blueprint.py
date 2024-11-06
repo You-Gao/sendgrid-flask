7a9ba03fbe1d9a958a12aa720b0f6775294fbec6
You Gao <djx3rn@virginia.edu>
2024-11-06 11:40:02 -0500

    chore: its coming together

--- blueprints/forward_blueprint.py
+++ blueprints/forward_blueprint.py
@@ -9,13 +9,26 @@ class EmailForward(MethodView):
     @bp.doc(description="Return pets based on ID", summary="Find pets by ID")
     @bp.response(200)
     def get(self):
-        response = make_response(render_template('forward.html'))
+        response = make_response(render_template('form.html'))
         response.headers['Content-Type'] = 'text/html'
         return response
     
-    
     @bp.response(201)
     def post(self):
-        url = request.form['url']
-        return redirect(url)
-    
+        headers = request.form.get('headers')
+        dkim = request.form.get('dkim')
+        content_ids = request.form.get('content-ids')
+        to = request.form.get('to')
+        text = request.form.get('text')
+        html = request.form.get('html')
+        from_email = request.form.get('from')
+        sender_ip = request.form.get('sender_ip')
+        spam_report = request.form.get('spam_report')
+        envelope = request.form.get('envelope')
+        attachments = request.form.get('attachments')
+        subject = request.form.get('subject')
+        spam_score = request.form.get('spam_score')
+        attachment_info = request.form.get('attachment-info')
+        charsets = request.form.get('charsets')
+        spf = request.form.get('SPF')
+        return redirect(url_for('forward.EmailForward'))
